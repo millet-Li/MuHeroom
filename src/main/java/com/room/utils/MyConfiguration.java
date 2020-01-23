e387e69a62f04cbee8872798d6b38e7501da22dc
millet-Li <1569702203@qq.com>
2020-01-23 16:15:39 +0800

    改了一点代码

--- src/main/java/com/room/utils/MyConfiguration.java
+++ src/main/java/com/room/utils/MyConfiguration.java
@@ -1,26 +1,27 @@
 package com.room.utils;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class MyConfiguration implements WebMvcConfigurer {
-    /*开发环境*/
+
+    @Value("${mydata.patha}")
+    private String image;
+    @Value("${mydata.pathb}")
+    private String life;
+    @Value("${mydata.pathc}")
+    private String room;
+//配置文件路径
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("file:/D:/room/image/");
-        registry.addResourceHandler("/life/**").addResourceLocations("file:/D:/room/life/");
-        registry.addResourceHandler("/room/**").addResourceLocations("file:/D:/room/room/");
+        registry.addResourceHandler("/image/**").addResourceLocations(image);
+        registry.addResourceHandler("/life/**").addResourceLocations(life);
+        registry.addResourceHandler("/room/**").addResourceLocations(room);
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
-    /*生产环境*/
-    /*@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("file:/C:/room/image/");
-        registry.addResourceHandler("/life/**").addResourceLocations("file:/C:/room/life/");
-        registry.addResourceHandler("/room/**").addResourceLocations("file:/C:/room/room/");
-        WebMvcConfigurer.super.addResourceHandlers(registry);
-    }*/
+
 }
